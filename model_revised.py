c82b1601b81f3d407bbd5c827dd747d1c9280ab9
Weiwu Yan <143419238+naivetoad@users.noreply.github.com>
2024-04-23 23:26:22 -0500

    Add files via upload

---
+++ model_revised.py
@@ -0,0 +1,126 @@
+import mesa
+
+
+class SchellingAgent(mesa.Agent):
+    """
+    Schelling segregation agent
+    """
+
+    def __init__(self, unique_id, model, agent_type, city_center):
+        """
+        Create a new Schelling agent.
+
+        Args:
+           unique_id: An agent's unique identifier.
+           x, y: An agent's initial location.
+           agent_type: An agent's type (minority=1, majority=0)
+           city_centers: the position of a city centers
+        """
+        super().__init__(unique_id, model)
+        self.type = agent_type
+        self.city_center = city_center
+
+    def step(self):
+        # Two city centers are initiated at the bottom-left corner and
+        # the top-right corner to maximize distance.
+        # Find the manhattan distance to the nearest city center 
+        distance_to_center = abs(self.pos[0] - self.city_center[0]) + abs(self.pos[1] - self.city_center[1])
+        travel_time = (distance_to_center * 1000)/ 30000 * 60 # in minutes
+        travel_utility = (30 - travel_time) # ranges from -322 to 30
+        if travel_utility < 0:
+            normalized_travel_utility = travel_utility / 322
+        else:
+            normalized_travel_utility = travel_utility / 30
+
+        similar = 0
+        unsimilar = 0
+        for neighbor in self.model.grid.iter_neighbors(self.pos, moore=True, radius=1):
+            # Find the number of similar and unsimilar neighbors
+            if neighbor.type == self.type:
+                similar += 1
+            else:
+                unsimilar += 1
+
+        homophily_utility = (similar - unsimilar) # ranges from -8 to 8
+        normalized_homophily_utility = homophily_utility / 8
+
+        total_utility = (normalized_travel_utility * self.model.preference_ratio) + normalized_homophily_utility
+        if total_utility > 0:
+            self.model.happy += 1
+            if normalized_travel_utility > 0:
+                self.model.happy_with_travel_time += 1
+            elif normalized_homophily_utility > 0:
+                self.model.happy_with_homophily += 1
+        else:
+            self.model.grid.move_to_empty(self)
+
+
+class Schelling(mesa.Model):
+    """
+    Model class for the Schelling segregation model.
+    """
+
+    def __init__(
+        self,
+        height=70,
+        width=60,
+        homophily=2,
+        density=0.7,
+        minority_pc=0.5,
+        preference_ratio = 1, 
+        seed=42,
+    ):
+        """
+        Create a new Schelling model.
+
+        Args:
+            width, height: Size of the space.
+            density: Initial Chance for a cell to populated
+            minority_pc: Chances for an agent to be in minority class
+            homophily: minimum ratio of number of similar agents to number of unsimilar agents
+            radius: Search radius for checking similarity
+            distance: required distance to a city center
+            seed: Seed for Reproducibility
+        """
+
+        super().__init__(seed=seed)
+        self.height = height
+        self.width = width
+        self.density = density
+        self.minority_pc = minority_pc
+        self.homophily = homophily
+        self.preference_ratio = preference_ratio
+
+        self.schedule = mesa.time.RandomActivation(self)
+        self.grid = mesa.space.SingleGrid(width, height, torus=False)
+
+        self.happy = 0
+        self.happy_with_travel_time = 0
+        self.happy_with_homophily = 0
+        self.datacollector = mesa.DataCollector(model_reporters={"happy": "happy", "happy_with_travel_time": "happy_with_travel_time", "happy_with_homophily": "happy_with_homophily"})
+
+        self.city_center = [52, 36]
+
+        # Set up agents
+        for _, pos in self.grid.coord_iter():
+            if self.random.random() < self.density:
+                agent_type = 1 if self.random.random() < self.minority_pc else 0
+                agent = SchellingAgent(self.next_id(), self, agent_type, self.city_center)
+                self.grid.place_agent(agent, pos)
+                self.schedule.add(agent)
+
+        self.datacollector.collect(self)
+
+    def step(self):
+        """
+        Run one step of the model.
+        """
+        self.happy = 0  # Reset counter of happy agents
+        self.happy_with_travel_time = 0
+        self.happy_with_homophily = 0
+        self.schedule.step()
+
+        self.datacollector.collect(self)
+
+        if self.happy == self.schedule.get_agent_count():
+            self.running = False
---
+++ server_revised.py
@@ -0,0 +1,70 @@
+import mesa
+from model import Schelling
+
+
+def get_happy_agents(model):
+    """
+    Display a text count of how many happy agents there are.
+    """
+    return f"Happy agents: {model.happy}; Agents happy with travel time: {model.happy_with_travel_time}; Agents happy with homophily: {model.happy_with_homophily}"
+
+
+def schelling_draw(agent):
+    """
+    Portrayal Method for canvas
+    """
+    # Portray generic cell
+    portrayal = {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Layer": 0, "Color": "white"}
+
+    # Differentiate portrayal for cities and agents
+    x, y = agent.pos if agent is not None else (None, None)
+    if agent.model.city_center == [x, y]:
+        portrayal["Color"] = "black"  # Set city center color to black
+        portrayal["Shape"] = "rect"  # Set shape to rectangle for city center
+        portrayal["w"] = 0.8  
+        portrayal["h"] = 0.8  
+        portrayal["Layer"] = 1  # Ensure city center is drawn above empty cells but below agents
+        return portrayal
+
+    # Portrayal for agents
+    if agent is not None:
+        portrayal = {"Shape": "circle", "r": 0.5, "Filled": "true", "Layer": 2}  # Agents are drawn on top
+        if agent.type == 0:
+            portrayal["Color"] = ["#FF0000", "#FF9999"]
+            portrayal["stroke_color"] = "#00FF00"
+        else:
+            portrayal["Color"] = ["#0000FF", "#9999FF"]
+            portrayal["stroke_color"] = "#000000"
+
+    return portrayal
+
+
+canvas_element = mesa.visualization.CanvasGrid(
+    portrayal_method=schelling_draw,
+    grid_width=60,
+    grid_height=70,
+    canvas_width=600,
+    canvas_height=700,
+)
+happy_chart = mesa.visualization.ChartModule([{"Label": "happy", "Color": "Black"}, {"Label": "happy_with_travel_time", "Color": "Blue"}, {"Label": "happy_with_homophily", "Color": "Green"}])
+
+model_params = {
+    "height": 70,
+    "width": 60,
+    "density": mesa.visualization.Slider(
+        name="Agent Density", value=0.7, min_value=0.1, max_value=1.0, step=0.1
+    ),
+    "minority_pc": mesa.visualization.Slider(
+        name="Minority Percentage", value=0.5, min_value=0.00, max_value=1.0, step=0.05
+    ),
+    "preference_ratio": mesa.visualization.Slider(
+        name="Preference Ratio", value = 1, min_value = 0, max_value=2, step=0.1
+    )
+}
+
+server = mesa.visualization.ModularServer(
+    model_cls=Schelling,
+    visualization_elements=[canvas_element, get_happy_agents, happy_chart],
+    name="Schelling Segregation Model",
+    model_params=model_params,
+)
