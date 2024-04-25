347bd68189751f914da13a7aceed8f4f7b61f5dd
Weiwu Yan <143419238+naivetoad@users.noreply.github.com>
2024-04-24 22:00:51 -0500

    Update server.py

--- server.py
+++ server.py
@@ -4,31 +4,31 @@ from model import Schelling
 
 def get_happy_agents(model):
     """
-    Display a text count of how many happy agents there are.
+    Display data collection in text
     """
     return f"Happy agents: {model.happy}; Agents happy with travel time: {model.happy_with_travel_time}; Agents happy with homophily: {model.happy_with_homophily}"
 
 
 def schelling_draw(agent):
     """
-    Portrayal Method for canvas
+    Portrayal method for canvas
     """
-    # Portray generic cell
+    # Portrayl for blocks
     portrayal = {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Layer": 0, "Color": "white"}
 
-    # Differentiate portrayal for cities and agents
+    # Portrayl for the city center
     x, y = agent.pos if agent is not None else (None, None)
     if agent.model.city_center == [(x, y)]:
-        portrayal["Color"] = "black"  # Set city center color to black
-        portrayal["Shape"] = "rect"  # Set shape to rectangle for city center
+        portrayal["Color"] = "black" 
+        portrayal["Shape"] = "rect"
         portrayal["w"] = 0.8  
         portrayal["h"] = 0.8  
-        portrayal["Layer"] = 1  # Ensure city center is drawn above empty cells but below agents
+        portrayal["Layer"] = 1  # Ensure the city center is displayed above agents
         return portrayal
 
     # Portrayal for agents
     if agent is not None:
-        portrayal = {"Shape": "circle", "r": 0.5, "Filled": "true", "Layer": 2}  # Agents are drawn on top
+        portrayal = {"Shape": "circle", "r": 0.5, "Filled": "true", "Layer": 0}
         if agent.type == 0:
             portrayal["Color"] = ["#FF0000", "#FF9999"]
             portrayal["stroke_color"] = "#00FF00"
@@ -39,6 +39,7 @@ def schelling_draw(agent):
     return portrayal
 
 
+# Set up the canvas
 canvas_element = mesa.visualization.CanvasGrid(
     portrayal_method=schelling_draw,
     grid_width=60,
@@ -46,10 +47,15 @@ canvas_element = mesa.visualization.CanvasGrid(
     canvas_width=600,
     canvas_height=700,
 )
+
+
+# Display data collection in a chart
 happy_chart = mesa.visualization.ChartModule([{"Label": "happy", "Color": "Black"}, 
                                               {"Label": "happy_with_travel_time", "Color": "Blue"}, 
                                               {"Label": "happy_with_homophily", "Color": "Green"}])
 
+
+# Set up modifiable paramters 
 model_params = {
     "height": 70,
     "width": 60,
@@ -64,6 +70,7 @@ model_params = {
     )
 }
 
+# Set up the server 
 server = mesa.visualization.ModularServer(
     model_cls=Schelling,
     visualization_elements=[canvas_element, get_happy_agents, happy_chart],
